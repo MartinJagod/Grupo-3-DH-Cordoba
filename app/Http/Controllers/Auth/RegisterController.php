ee53275e102864f3dfbac2f2b73b1be7f00eff19
Martin Jagod <jagodnikmartin@gmail.com>
2019-11-24 16:58:26 -0300

    solucion conexion a BD y agregue campos de registros

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,8 +50,13 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
+            'surname' => ['required', 'string', 'max:255'],
+            'status'=>['boolean'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'photo' => ['string', 'max:255'],
+            'phone' => ['string', 'max:255'],
+
         ]);
     }
 
@@ -65,6 +70,10 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'surname' => $data['surname'],
+            'status'=>true,
+            'photo'=>"Esta es una foto",
+            'phone'=>$data['phone'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
