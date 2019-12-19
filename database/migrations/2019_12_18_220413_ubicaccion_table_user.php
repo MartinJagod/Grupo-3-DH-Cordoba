b1d4f398f5f7d3d0c4329933047fc26c3f912219
Martin Jagod <jagodnikmartin@gmail.com>
2019-12-18 23:17:27 -0300

    cambios en ubicacion

--- database/migrations/2019_12_18_220413_ubicaccion_table_user.php
+++ database/migrations/2019_12_18_220413_ubicaccion_table_user.php
@@ -14,7 +14,7 @@ class UbicaccionTableUser extends Migration
     public function up()
     {
       Schema::table('users', function (Blueprint $table) {
-$table->string('ubicacion');
+$table->string('ubicacion')->nullable();;
 });
 }
     /**
